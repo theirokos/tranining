888b018e461815e5f61fd8d7228da05503cfc7b7
theirokos <irokopaul778@gmail.com>
2023-11-03 14:12:45 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-alert("Hello World!")
-undefined
-let js = 'amazing'
-undefined
-if (js === 'amazing') alert('JavaScript is FUN!')
-undefined
-js = 'boring'
-'boring'
-if (js === 'amazing') alert('JavaScript is FUN!')
-undefined
-40 + 8 + 23 - 10
-61
+var firstName = 'paul';
