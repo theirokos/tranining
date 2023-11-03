f687edb9141fb328f7ecac52ec7988340420aa3b
theirokos <irokopaul778@gmail.com>
2023-11-03 13:58:04 +0100

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var firstName = 'paul';
+alert("Hello World!")
+undefined
+let js = 'amazing'
+undefined
+if (js === 'amazing') alert('JavaScript is FUN!')
+undefined
+js = 'boring'
+'boring'
+if (js === 'amazing') alert('JavaScript is FUN!')
+undefined
+40 + 8 + 23 - 10
+61
