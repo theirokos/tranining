b2b610289aa7ec517eb9fbcd790f626f6152b1c5
theirokos <irokopaul778@gmail.com>
2023-11-06 15:20:36 +0100

    First commit

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Maltilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'Jonas'
+let PI = 3.1415
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
